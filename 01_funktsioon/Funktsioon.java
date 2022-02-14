590aea4ef2d60a55ee2a606126af0d6c66174620
Kevin Rosenberg <kevin.rosenberg@tlu.ee>
2022-02-14 12:18:56 +0200

    kodutöö

--- 01_funktsioon/Funktsioon.java
+++ 01_funktsioon/Funktsioon.java
@@ -1,15 +1,17 @@
 public class Funktsioon {
 	public static int tunde(int tunnid){
-		return 24-tunnid;
+		return 23-tunnid; // 24 tunni asemel 23 tundi, sest minutit lisavad aega juurde
 	}
 	public static int minuteid(int minutid){
 		return 60-minutid;
 	}
 	public static void main(String[] args) {
-		if (args.length>1){
+		if (args.length>1){ // kui on sisestatud rohkem kui 1 argument
 			int tund=Integer.parseInt(args[0]); // tundide sisestus int
 			int minut=Integer.parseInt(args[1]); // minutite sisestus int
 			System.out.println(tunde(tund) + " tundi " + minuteid(minut) + " minutit keskööni.");
+		} else {
+			System.out.println("Lisa tunnid ja minutid!");
 		}
 	}
 }
